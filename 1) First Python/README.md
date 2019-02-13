0c86fe6a1157f974f13178e5c91b5ff362ea49ab
TimTree <TimTree@users.noreply.github.com>
2019-02-12 23:53:09 -0800

    Add First Python

---
+++ 1) First Python/README.md
@@ -0,0 +1,12 @@
+# Python Musings - First Python
+Here's my first Python script I wrote back in April 2013 (found on a flash drive). Enter your age, and find out what the script thinks about you.
+
+## Python Objectives
+* Print to console
+* User input
+* Casting
+* If statement
+
+There's NO error checking for non-numeric inputs. I would've added that now, but I'd like to keep the script true to my 2013 self.
+
+The same goes for not using else if statements (elif).
---
+++ 1) First Python/firstpython.py
@@ -0,0 +1,22 @@
+print ("hello player!")
+age = int(input ("What is your age? "))
+if age < 0:
+     print ("duh wut")
+if age >= 0 and age <= 4:
+     print ("Wow, you are super young.")
+if age >= 5 and age <= 9:
+     print ("Hello, kid.")
+if age >= 10 and age <= 12:
+     print ("Hello, preteen.")
+if age >= 13 and age <= 17:
+     print ("Hello, teenager.")
+if age >= 18 and age <= 21:
+     print ("Hello, young adult.")
+if age >= 22 and age <= 59:
+     print ("Hello, adult.")
+if age >= 60 and age <= 89:
+     print ("Hello, old dude.")
+if age >= 90 and age <= 109:
+     print ("Wow, you can type? Wow.")
+if age >= 110:
+     print ("I respect your age, wise one.")
